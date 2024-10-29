a946906109cad3ec17518babdffd2ff1ed90d2ab
gilbert <gilbertoforiboye@gmail.com>
2024-10-29 13:25:05 +0000

    Work done

---
+++ src/components/NutritionTable.jsx
@@ -0,0 +1,36 @@
+// src/components/NutritionTable.js
+import React from "react";
+import "./NutritionTable.css";
+
+const NutritionTable = () => {
+  return (
+    <section className="nutrition">
+      <h2>Nutrition</h2>
+      <p>
+        The table below shows nutritional values per serving without the additional fillings.
+      </p>
+      <table>
+        <tbody>
+          <tr>
+            <td>Calories</td>
+            <td><strong>277 kcal</strong></td>
+          </tr>
+          <tr>
+            <td>Carbs</td>
+            <td><strong>0 g</strong></td>
+          </tr>
+          <tr>
+            <td>Protein</td>
+            <td><strong>20 g</strong></td>
+          </tr>
+          <tr>
+            <td>Fat</td>
+            <td><strong>22 g</strong></td>
+          </tr>
+        </tbody>
+      </table>
+    </section>
+  );
+};
+
+export default NutritionTable;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
+import RecipeCard from './components/RecipeCard.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RecipeCard/>
   </StrictMode>,
 )
